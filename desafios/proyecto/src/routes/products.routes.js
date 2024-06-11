690e23b85378f624af83ff110d7fcfc4e5b5fb6d
Edoardo Maciel <edoardo.maciel@gmail.com>
2024-06-10 18:59:59 -0600

    Faltaban rutas en products.routes.js

--- desafios/proyecto/src/routes/products.routes.js
+++ desafios/proyecto/src/routes/products.routes.js
@@ -9,13 +9,39 @@ router.get('/', (req, res) => {
   res.json(products);
 });
 router.get('/:pid', (req, res) => {
-  res.json(products);
+  const pid = parseInt(req.params.id);
+  const product = products.find((product) => product.id === pid);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send('Producto no encontrado');
+  }
 });
 
 router.post('/', (req, res) => {
-  const newProduct = req.body;
+  const { title, description, code, price } = req.body;
+  if (!title || !description || !code || !price) {
+    res.status(400).send('Faltan datos');
+    return;
+  }
+  const newProduct = {
+    title,
+    description,
+    code,
+    price,
+  };
   products.push(newProduct);
   res.send('Producto creado correctamente');
+});
+
+router.delete('/:pid', (req, res) => {
+  const pid = parseInt(req.params.id);
+  const product = products.find((product) => product.id === pid);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send('Producto no encontrado');
+  }
 });
 //EXPORTAMOS LAS RUTAS
 export default router;
